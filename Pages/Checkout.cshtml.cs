ff0e44ce63f3885624af1e9eff1266d0efde46a6
Xiao Hu <xiao@squareup.com>
2018-05-04 13:06:46 -0700

    Init sample project

---
+++ Pages/Checkout.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Square.Connect.Api;
+using Square.Connect.Model;
+
+namespace sqRazorSample.Pages
+{
+    public class CheckoutModel : PageModel
+    {
+        public CatalogObject Item { get; set; }
+        public string ApplicationId { get; set; }
+        public string LocationId { get; set; }
+
+        public String ItemName {
+            get {
+                return this.Item.ItemVariationData.ItemId;
+            }
+        }
+
+        public bool IsSandbox { get; set; }
+
+        public string PriceAmountString {
+            get {
+                return this.Item.ItemVariationData.PriceMoney.Amount.ToString();
+            }
+        }
+
+        public long? PriceAmount
+        {
+            get
+            {
+                return this.Item.ItemVariationData.PriceMoney.Amount;
+            }
+        }
+
+        public CheckoutModel(IConfiguration configuration)
+        {
+            this.ApplicationId = configuration["AppSettings:ApplicationId"];
+            this.LocationId = configuration["AppSettings:LocationId"];
+
+            this.IsSandbox = (configuration["AppSettings:Sandbox"] != null && configuration["AppSettings:Sandbox"].ToLower() == "true") ?
+                true : false;
+        }
+
+        public void OnGet()
+        {
+            if (this.IsSandbox)
+            {
+                this.Item = new CatalogObject(CatalogObject.TypeEnum.ITEM, "Sandbox");
+                this.Item.ItemVariationData = new CatalogItemVariation();
+                this.Item.ItemVariationData.PriceMoney = new Money(100, Money.CurrencyEnum.USD);
+                this.Item.ItemVariationData.ItemId = "SandboxItem";
+            }
+            else
+            {
+                string itemVarId = Request.Query["itemVarId"];
+                CatalogApi catalogApi = new CatalogApi();
+                RetrieveCatalogObjectResponse res = catalogApi.RetrieveCatalogObject(itemVarId, true);
+                this.Item = res._Object;
+            }
+        }
+
+        public string FormatMoney(long? amount)
+        {
+            return string.Format("${0:F2}", amount / 100.0);
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Square.Connect.Api;
+using Square.Connect.Model;
+
+namespace sqRazorSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public bool IsSandbox { get; set; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this.IsSandbox = (configuration["AppSettings:Sandbox"] != null && configuration["AppSettings:Sandbox"].ToLower() == "true") ?
+                true : false;
+        }
+
+        public List<CatalogObject> Items { get; set; }
+
+        public void OnGet()
+        {
+            if (this.IsSandbox)
+            {
+                // We'll display a dummy item on ui
+                return;
+            }
+
+            CatalogApi catalogApi = new CatalogApi();
+            ListCatalogResponse res = catalogApi.ListCatalog(null, "ITEM");
+
+            this.Items = new List<CatalogObject>();
+            this.Items.AddRange(res.Objects);
+        }
+
+        public string FormatMoney(long? amount)
+        {
+            return string.Format("${0:F2}", amount / 100.0);
+        }
+    }
+}
